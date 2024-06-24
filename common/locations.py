490fb9aac774efce5142a58e2793cd6c5ee59e3f
Michael Franzonello <mfranzonello@gmail.com>
2024-06-24 12:14:00 -0400

    another typo

--- common/locations.py
+++ common/locations.py
@@ -6,4 +6,4 @@ SPOTIFY_PLAY_URL = 'https://open.spotify.com'
 SPOTIFY_AUTH_URL = 'https://accounts.spotify.com'
 MUSICBRAINZ_URL = 'https://musicbrainz.org/ws/2'
 SONOS_LOGIN_URL = 'https://api.sonos.com/login/v3'
-SONOS_CONTROL_URL = 'https://api.ws.sonos.com/control/api
+SONOS_CONTROL_URL = 'https://api.ws.sonos.com/control/api'
