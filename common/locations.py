a6fca7745de5f5a0c84ddc86956de3e3d2f03361
Michael Franzonello <mfranzonello@gmail.com>
2024-06-24 12:03:34 -0400

    fixed typo with ''

--- common/locations.py
+++ common/locations.py
@@ -5,7 +5,7 @@ GITHUB_URL = 'https://api.github.com'
 SPOTIFY_PLAY_URL = 'https://open.spotify.com'
 SPOTIFY_AUTH_URL = 'https://accounts.spotify.com'
 MUSICBRAINZ_URL = 'https://musicbrainz.org/ws/2'
-SONOS_LOGIN_URL = ''https://api.sonos.com/login/v3'
+SONOS_LOGIN_URL = 'https://api.sonos.com/login/v3'
 SONOS_CONTROL_URL = 'https://api.ws.sonos.com/control/api/v1'
 
 # redirects and URIs
